74f4a4f096a8670c375a6d2a3070fa4894bb6f45
Joe Hunt <josephb.hunt@gmail.com>
2018-02-21 00:19:00 -0500

    Added movement and rocket thrust sound.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if(Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.back);
+        }
+    }
+}
